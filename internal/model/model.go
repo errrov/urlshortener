36469e19b05cd9feb6499d41337f0394fa162e40
e1vo <deihp6@gmail.com>
2023-06-05 21:04:37 +0300

    Completed models for go project

--- internal/model/model.go
+++ internal/model/model.go
@@ -4,9 +4,15 @@ import "errors"
 
 var (
 	ErrNotFound = errors.New("not found")
+	ErrIdExist  = errors.New("identifier already exists")
 )
 
 type Shortened struct {
 	Identifier string `json:"identifier"`
-	Original string `json:"original_url"`
+	Original   string `json:"original_url"`
+}
+
+type UserInput struct {
+	OriginalURL string
+	Identifier  string
 }
