8b27c1b60ea388627634ef54ec0dd9a1d0b9a8f1
e1vo <deihp6@gmail.com>
2023-06-05 21:03:56 +0300

    Completed in_memory realisation of memory storage for tests / main

---
+++ internal/storage/in_memory/in_memory.go
@@ -0,0 +1,34 @@
+package in_memory
+
+import (
+	"sync"
+
+	"github.com/errrov/urlshortenerozon/internal/model"
+)
+
+type inMemory struct {
+	m sync.Map
+}
+
+func NewInMemory() *inMemory {
+	return &inMemory{}
+}
+
+func (s *inMemory) Add(shortened model.Shortened) (*model.Shortened, error) {
+	if _, exist := s.m.Load(shortened.Identifier); exist {
+		return nil, model.ErrIdExist
+	}
+	s.m.Store(shortened.Identifier, shortened)
+	return &shortened, nil
+}
+
+func (s *inMemory) Get(identifier string) (*model.Shortened, error) {
+	v, ok := s.m.Load(identifier)
+	if !ok {
+		return nil, model.ErrNotFound
+	}
+	shortening := v.(model.Shortened)
+	return &shortening, nil
+}
+
+
